6a34c63688dfc092bd519e10b8a30043422f9350
alex chan <chan.k.leong@gmail.com>
2017-02-04 14:27:22 +0800

    Initial commit

---
+++ basics/cmdline_args.go
@@ -0,0 +1,21 @@
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+
+func main() {
+
+	// The os package provides function and values for dealing with the operating system.
+	// Program argument are availabe to the os.Args variable. It is a slice.
+	// os.Args[0] is the name of the program itself.
+	fmt.Println("Name of program: " + os.Args[0])
+
+	fmt.Println("The arguments:")
+	for i:=1; i < len(os.Args); i++ {
+		fmt.Println(os.Args[i])
+	}
+}
---
+++ basics/hello_world.go
@@ -0,0 +1,14 @@
+
+// This is the standard hello world program.
+
+
+// Package main is special. It defines a standalone program.
+package main
+
+//This lime imports the "fmt" package.
+import "fmt"
+
+//Within package main, main() is special. Whatever it does, is what the program does.
+func main() {
+	fmt.Println("Hello, world!")
+}
